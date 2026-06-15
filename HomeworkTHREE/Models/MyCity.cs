cb88d7d296c2ba33e7c44b80c148c89301001798
avelas68 <47703183+avelas68@users.noreply.github.com>
2026-06-14 21:08:54 -0700

    Add files via upload

---
+++ HomeworkTHREE/Models/MyCity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace HomeworkTHREE.Models
+{
+    public class MyCity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public int Cases { get; set; }
+        public int Deaths { get; set; }
+        public int Tested { get; set; }
+        public string Date { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"[{Id}, {Name} {Population}, {Cases}, {Deaths}, {Tested}, {Date}]";
+        }
+    }
+}
