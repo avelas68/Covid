b5a5111d03ec09b10fc7b2ede7c9ec4cfb1934d4
avelas68 <47703183+avelas68@users.noreply.github.com>
2026-06-14 20:15:18 -0700

    Add files via upload

---
+++ Controllers/FIlesController.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using HomeworkTHREE.Services;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace HomeworkTHREE.Controllers
+//{
+//    public class FileController : Controller
+//    {
+//      //  private readonly FileServices FileServices;
+
+//        //public FileController(FileServices FileServices)
+//        //{
+//        //    this.FileServices = FileServices;
+//        //}
+//        [HttpGet]
+//        public IActionResult Privacy()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public IActionResult Upload(IFormFile file)
+//        {
+//            using var reader = new StreamReader(file.OpenReadStream());
+//            var lines = new List<string>();
+//            string line;
+//            while ((line = reader.ReadLine()) != null)
+//                lines.Add(line);
+           
+
+//            return View();
+//        }
+
+//    }
+//}
