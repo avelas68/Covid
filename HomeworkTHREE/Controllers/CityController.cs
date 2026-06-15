446ac4d753c385ec4a46eab5d350b77f9d45e7e2
avelas68 <47703183+avelas68@users.noreply.github.com>
2026-06-14 21:08:30 -0700

    Add files via upload

---
+++ HomeworkTHREE/Controllers/CityController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using HomeworkTHREE.Models;
+using HomeworkTHREE.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkTHREE.Controllers
+{
+    public class CityController : Controller
+    {
+        private readonly CityServices CityServices;
+
+        public CityController(CityServices CityServices)
+        {
+            this.CityServices = CityServices;
+        }
+
+        public IActionResult List()
+        {
+            return View(CityServices.GetMyCities());
+        }
+    }
+}
